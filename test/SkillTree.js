462da12a760e2d328380e704f922a73f015496fb
Timothée Gaultier <tiimogo@gmail.com>
2024-01-09 16:45:58 +0100

    f

--- test/SkillTree.js
+++ test/SkillTree.js
@@ -1,21 +1,35 @@
-const {expect} = require("chai");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe("SkillTree", function () {
-    async function deploySkillTree() {
-        // Contracts are deployed using the first signer/account by default
-        const [owner, otherAccount] = await ethers.getSigners();
+describe("SkillTree Contract", function () {
+    let SkillTree;
+    let owner, user1, user2;
 
-        const SkillTree = await ethers.getContractFactory("SkillTree");
-        const lock = await SkillTree.deploy();
-
-        return {lock, owner, otherAccount};
-    }
-
-    describe("Deployment", function () {
-        it("Should deploy without failing", async function () {
-            const {lock, owner} = await deploySkillTree();
-            expect(lock).to.not.equal(undefined);
-        });
+    before(async function () {
+        [owner, user1, user2] = await ethers.getSigners();
+        SkillTree = await ethers.getContractFactory("SkillTree");
     });
 
+    it('Should exclude the caller from the user list', async function () {
+        const skillTree = await SkillTree.deploy();
+        await skillTree.addUser(user1.address, "User1FirstName", "User1LastName");
+        await skillTree.addUser(user2.address, "User2FirstName", "User2LastName");
+
+        // Get the list of users
+        const users = await skillTree.listUsers();
+        expect(users).to.not.include(owner.address);
+    });
+
+    it('Should include all other users except the caller', async function () {
+        // Deploy the contract
+        const skillTree = await SkillTree.deploy();
+        await skillTree.addUser(user1.address, "User1FirstName", "User1LastName");
+        await skillTree.addUser(user2.address, "User2FirstName", "User2LastName");
+
+
+        const users = await skillTree.listUsers();
+        expect(users).to.include(user1.address);
+        expect(users).to.include(user2.address);
+        expect(users.length).to.equal(2);
+    });
 });
