21e7c68ae52342ea73078e4ce315eca72811078e
Timothée DURAND <timothee.durand70@gmail.com>
2024-01-09 14:44:13 +0100

    feat: contract base

---
+++ contracts/SkillTree.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+
+contract SkillTree {
+    struct Skill {
+        string name;
+        uint level;
+    }
+
+    struct Profile {
+        string lastName;
+        string firstName;
+        //address walletAddress;
+    }
+
+    struct SkillValidation {
+        address validator;
+        uint32 skillId;
+    }
+
+    mapping(address => Profile) private profiles;
+    mapping(address => mapping(uint32 => Skill)) private skills;
+    mapping(address => SkillValidation) private skillsValidation;
+
+
+    function listUsers() public view returns (Profile[] memory) {
+        //TODO : fill this
+    }
+
+    function getUserSkills(address _address) public view returns (Skill[] memory) {
+        //TODO : fill this
+    }
+
+    function getUser(address _address) public view returns (Profile memory) {
+        // TODO: fill this
+    }
+
+    function editProfile(string memory _lastName, string memory _firstName) public {
+        //TODO: fill this
+    }
+
+    function getUser(address _address) public view returns (Profile memory) {
+        // TODO: fill this
+    }
+
+    function addSkill(string memory _name, uint memory _level) public {
+        //TODO: fill this
+    }
+
+    function editSkill(uint32 _skillId, Skill _newSkill) public  {
+        //TODO: fill this
+    }
+
+    function deleteSkill(uint32 _skillId) public  {
+        //TODO: fill this
+    }
+}
