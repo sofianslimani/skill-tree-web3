4e022848cfc575017df10abf5f7dfb8d3c1c9b8f
Timothée DURAND <timothee.durand70@gmail.com>
2024-01-10 10:19:36 +0100

    feat: delete a skill

--- contracts/SkillTree.sol
+++ contracts/SkillTree.sol
@@ -1,24 +1,24 @@
-    // SPDX-License-Identifier: UNLICENSED
-    pragma solidity ^0.8.9;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
 
 import "hardhat/console.sol";
 
-    contract SkillTree {
-        struct Skill {
-            string name;
-            uint level;
-        }
+contract SkillTree {
+    struct Skill {
+        string name;
+        uint level;
+    }
 
-        struct Profile {
-            string lastName;
-            string firstName;
-            //address walletAddress;
-        }
+    struct Profile {
+        string lastName;
+        string firstName;
+        //address walletAddress;
+    }
 
-        struct SkillValidation {
-            address validator;
-            uint32 skillId;
-        }
+    struct SkillValidation {
+        address validator;
+        uint32 skillId;
+    }
 
     struct SkillDto {
         string name;
@@ -30,75 +30,81 @@ import "hardhat/console.sol";
     mapping(address => Skill[]) private skills;
     mapping(address => SkillValidation[]) private skillsValidation;
 
-        constructor() {}
+    constructor() {}
 
     function getUserSkills(address _address) public view returns (SkillDto[] memory) {
-        SkillValidation[] memory userSkillsValidation = skillsValidation[_address];
         SkillDto[] memory userSkills = new SkillDto[](skills[_address].length);
         for (uint i = 0; i < skills[_address].length; i++) {
             Skill memory skill = skills[_address][i];
-            uint  resultCount = 0;
-            for (uint j = 0; j < userSkillsValidation.length; j++) {
-                SkillValidation memory skillValidation = userSkillsValidation[j];
-                if (skillValidation.skillId == i) {
-                    resultCount++;
-                }
-            }
-            SkillValidation[] memory skillValidations = new SkillValidation[](resultCount);
-            uint resultIndex = 0;
-            for (uint j = 0; j < userSkillsValidation.length; j++) {
-                SkillValidation memory skillValidation = userSkillsValidation[j];
-                if (skillValidation.skillId == i) {
-                    skillValidations[resultIndex] = skillValidation;
-                    resultIndex++;
-                }
-            }
             userSkills[i] = SkillDto({
                 name: skill.name,
                 level: skill.level,
-                validations: skillValidations
+                validations: filterSkillValidation(_address, uint32(i))
             });
         }
         return userSkills;
-        }
-        
-        address[] private userAddresses;
+    }
 
-        function addUser(address _userAddress, string memory _firstName, string memory _lastName) public {
-            // Ajouter le profil
-            profiles[_userAddress] = Profile(_lastName, _firstName);
+    function getUserSkillValidations(address _address) public view returns (SkillValidation[] memory) {
+        return skillsValidation[_address];
+    }
 
-            // Ajouter l'adresse à la liste des adresses
-            userAddresses.push(_userAddress);
-        }
-
-        function listUsers() public view returns (address[] memory) {
-            uint count = 0;
-
-            // Count how many users are not the caller
-            for (uint i = 0; i < userAddresses.length; i++) {
-                if (userAddresses[i] != msg.sender) {
-                    count++;
-                }
+    function filterSkillValidation(address _address, uint32 _skillId) private view returns (SkillValidation[] memory) {
+        SkillValidation[] memory userSkillsValidation = skillsValidation[_address];
+        uint resultCount = 0;
+        for (uint j = 0; j < userSkillsValidation.length; j++) {
+            SkillValidation memory skillValidation = userSkillsValidation[j];
+            if (skillValidation.skillId == _skillId) {
+                resultCount++;
             }
-
-            address[] memory nonCallerUsers = new address[](count);
-            uint j = 0;
-
-            // Populate the array with users that are not the caller
-            for (uint i = 0; i < userAddresses.length; i++) {
-                if (userAddresses[i] != msg.sender) {
-                    nonCallerUsers[j] = userAddresses[i];
-                    j++;
-                }
+        }
+        SkillValidation[] memory skillValidations = new SkillValidation[](resultCount);
+        uint resultIndex = 0;
+        for (uint j = 0; j < userSkillsValidation.length; j++) {
+            SkillValidation memory skillValidation = userSkillsValidation[j];
+            if (skillValidation.skillId == _skillId) {
+                skillValidations[resultIndex] = skillValidation;
+                resultIndex++;
             }
+        }
+        return skillValidations;
+    }
 
-            return nonCallerUsers;
+    address[] private userAddresses;
+
+    function addUser(address _userAddress, string memory _firstName, string memory _lastName) public {
+        // Ajouter le profil
+        profiles[_userAddress] = Profile(_lastName, _firstName);
+
+        // Ajouter l'adresse à la liste des adresses
+        userAddresses.push(_userAddress);
+    }
+
+    function listUsers() public view returns (address[] memory) {
+        uint count = 0;
+
+        // Count how many users are not the caller
+        for (uint i = 0; i < userAddresses.length; i++) {
+            if (userAddresses[i] != msg.sender) {
+                count++;
+            }
         }
 
+        address[] memory nonCallerUsers = new address[](count);
+        uint j = 0;
 
+        // Populate the array with users that are not the caller
+        for (uint i = 0; i < userAddresses.length; i++) {
+            if (userAddresses[i] != msg.sender) {
+                nonCallerUsers[j] = userAddresses[i];
+                j++;
+            }
+        }
 
-        //
+        return nonCallerUsers;
+    }
+
+    //
 //    function getUserSkills(address _address) public view returns (Skill[] memory) {
 //        //TODO : fill this
 //    }
@@ -123,10 +129,49 @@ import "hardhat/console.sol";
 //        //TODO: fill this
 //    }
 //
-//    function deleteSkill(uint32 _skillId) public  {
-//        //TODO: fill this
-//    }
-//
+    function deleteSkill(uint32 _skillId) public  {
+        require(_skillId < skills[msg.sender].length, "Skill does not exist");
+
+        // Shift elements to the left
+        for (uint i = _skillId; i < skills[msg.sender].length - 1; i++) {
+            skills[msg.sender][i] = skills[msg.sender][i + 1];
+        }
+
+        // Delete the last element
+        skills[msg.sender].pop();
+
+        // Do the same for skillsValidation
+        for (uint i = 0; i < skillsValidation[msg.sender].length; i++) {
+            if (skillsValidation[msg.sender][i].skillId == _skillId) {
+                for (uint j = i; j < skillsValidation[msg.sender].length - 1; j++) {
+                    skillsValidation[msg.sender][j] = skillsValidation[msg.sender][j + 1];
+                }
+                skillsValidation[msg.sender].pop();
+                i--; // Decrement i as the array length has decreased
+            }
+        }
+    }
+
+    function filterSkills(uint32 _skillId) private view returns(Skill[] memory) {
+        uint resultCount = 0;
+        for (uint i = 0; i < skills[msg.sender].length; i++) {
+            Skill memory skill = skills[msg.sender][i];
+            if (i != _skillId) {
+                resultCount++;
+            }
+        }
+        Skill[] memory result = new Skill[](resultCount);
+        uint resultIndex = 0;
+        for (uint i = 0; i < skills[msg.sender].length; i++) {
+            Skill memory skill = skills[msg.sender][i];
+            if (i != _skillId) {
+                result[resultIndex] = skill;
+                resultIndex++;
+            }
+        }
+        return result;
+    }
+
     function addSkillValidation(address _userAddress, uint32 _skillId) public {
         require(msg.sender != _userAddress, "You cannot validate your own skills");
         require(_skillId < skills[_userAddress].length, "Skill does not exist");
--- test/SkillTree.js
+++ test/SkillTree.js
@@ -64,6 +64,24 @@ describe("SkillTree", function () {
             const skills = await skillTree.getUserSkills(owner.address);
             expect(skills).to.eql([]);
         });
+
+        it('should delete the skill and theses validation if it exist', async function () {
+            const { skillTree, owner } = await deploySkillTree();
+            await skillTree.addSkill('javascript', 3);
+            await skillTree.deleteSkill(0);
+            const skills = await skillTree.getUserSkills(owner.address);
+            expect(skills).to.eql([]);
+            const skillValidations = await skillTree.getUserSkillValidations(owner.address);
+            expect(skillValidations).to.eql([]);
+        })
+
+        it('should fail to delete a skill if the skill does not exist', async function () {
+            const { skillTree, owner } = await deploySkillTree();
+            async function deleteSkill(skillId) {
+                return await skillTree.deleteSkill(skillId);
+            }
+            await expect(deleteSkill(0)).to.be.revertedWith('Skill does not exist');
+        })
     });
 
 
