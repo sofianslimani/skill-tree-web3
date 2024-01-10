a73d491b21d8618c8382c22ff5467effc7635796
Sofian Slimani <sofianslimani48@gmail.com>
2024-01-10 10:13:07 +0100

    fix: re-add Metamask to HomePage

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,20 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
 import styles from "./HomePage.module.scss";
+import { MetamaskContext, MetamaskContextType } from "../../App";
+import { Link } from "react-router-dom";
 
 const fakeUsers = [
   { id: 1, username: "UserOne", profession: "Développeur Frontend" },
   { id: 2, username: "UserTwo", profession: "Concepteur UX" },
+  { id: 3, username: "UserTwo", profession: "Concepteur UX" },
+  { id: 4, username: "UserTwo", profession: "Concepteur UX" },
+  { id: 5, username: "UserTwo", profession: "Concepteur UX" },
 ];
 
 const HomePage = () => {
+  const metamaskContext: MetamaskContextType = useContext(MetamaskContext)!;
+
+  console.log(metamaskContext);
   return (
     <div className={styles.homePage}>
       <h1>Liste des Utilisateurs</h1>
