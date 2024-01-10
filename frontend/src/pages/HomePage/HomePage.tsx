aff6c759ec84e8f989863b904771f38e2d434963
Sofian Slimani <sofianslimani48@gmail.com>
2024-01-10 10:08:33 +0100

    feat: modify HomePage for add users list

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,26 @@
-import { useContext } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./HomePage.module.scss";
-import { MetamaskContext, MetamaskContextType } from "../../App";
+
+const fakeUsers = [
+  { id: 1, username: "UserOne", profession: "Développeur Frontend" },
+  { id: 2, username: "UserTwo", profession: "Concepteur UX" },
+];
 
 const HomePage = () => {
-  const metamaskContext: MetamaskContextType = useContext(MetamaskContext)!;
-
-  console.log(metamaskContext);
-
   return (
     <div className={styles.homePage}>
-      <h1 className={styles.title}>Bienvenue sur la page d'accueil</h1>
-      <p>Bienvenue sur mon site web construit avec React et TypeScript.</p>
+      <h1>Liste des Utilisateurs</h1>
+      <ul className={styles.userList}>
+        {fakeUsers.map((user) => (
+          <li key={user.id} className={styles.userItem}>
+            <Link to={`/profile/${user.id}`} className={styles.userLink}>
+              {user.username}
+            </Link>
+            <div className={styles.profession}>{user.profession}</div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
