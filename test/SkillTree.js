2fbd364b432a81d21210a04c6011e09b1c265e66
timogo <44207397+timoogo@users.noreply.github.com>
2024-01-10 09:51:48 +0100

    feat/add-user-and-listUser (#26)
    Co-authored-by: Sofian Slimani <70762272+sofianslimani@users.noreply.github.com>

--- test/SkillTree.js
+++ test/SkillTree.js
@@ -1,48 +1,68 @@
-const {expect} = require("chai");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("SkillTree", function () {
     async function deploySkillTree() {
-        // Contracts are deployed using the first signer/account by default
-        const [owner, otherAccount] = await ethers.getSigners();
-
+        const [owner, user1, user2] = await ethers.getSigners();
         const SkillTree = await ethers.getContractFactory("SkillTree");
         const skillTree = await SkillTree.deploy();
 
-        return {skillTree, owner, otherAccount};
+        return { skillTree, owner, user1, user2 };
     }
 
     describe("Deployment", function () {
         it("Should deploy without failing", async function () {
-            const {skillTree} = await deploySkillTree();
+            const { skillTree } = await deploySkillTree();
             expect(skillTree).to.not.equal(undefined);
         });
+    });
 
+    describe("User Management", function () {
+        it('Should exclude the caller from the user list', async function () {
+            const { skillTree, owner, user1, user2 } = await deploySkillTree();
+            await skillTree.addUser(user1.address, "User1FirstName", "User1LastName");
+            await skillTree.addUser(user2.address, "User2FirstName", "User2LastName");
+
+            const users = await skillTree.listUsers();
+            expect(users).to.not.include(owner.address);
+        });
+
+        it('Should include all other users except the caller', async function () {
+            const { skillTree, user1, user2 } = await deploySkillTree();
+            await skillTree.addUser(user1.address, "User1FirstName", "User1LastName");
+            await skillTree.addUser(user2.address, "User2FirstName", "User2LastName");
+
+            const users = await skillTree.listUsers();
+            expect(users).to.include(user1.address);
+            expect(users).to.include(user2.address);
+            expect(users.length).to.equal(2);
+        });
     });
 
     describe("Skills", function () {
         it("Should list the users skills", async function () {
-            const {skillTree, owner} = await deploySkillTree();
-           const skills = await skillTree.getUserSkills(owner.address)
+            const { skillTree, owner } = await deploySkillTree();
+            const skills = await skillTree.getUserSkills(owner.address);
             expect(skills).to.eql([]);
         });
+
         it("Should add a skill to the user", async function () {
-            const {skillTree, owner} = await deploySkillTree();
+            const { skillTree, owner } = await deploySkillTree();
             await skillTree.addSkill('javascript', 3);
-            const skills = await skillTree.getUserSkills(owner.address)
+            const skills = await skillTree.getUserSkills(owner.address);
             expect(skills).to.eql([['javascript', BigInt(3)]]);
         });
+
         it('should fail to add a skill if the skill level is <0 or >5', async function () {
-            const {skillTree, owner} = await deploySkillTree();
+            const { skillTree, owner } = await deploySkillTree();
             async function addSkill(skill, level) {
                 return await skillTree.addSkill(skill, level);
             }
             await expect(addSkill('javascript', -1)).to.be.rejected;
             await expect(addSkill('javascript', 0)).to.be.revertedWith('Skill level must be between 1 and 5');
             await expect(addSkill('javascript', 6)).to.be.revertedWith('Skill level must be between 1 and 5');
-            const skills = await skillTree.getUserSkills(owner.address)
+            const skills = await skillTree.getUserSkills(owner.address);
             expect(skills).to.eql([]);
-        })
+        });
     });
-
-
 });
